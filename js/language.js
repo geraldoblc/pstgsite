bb20b7d49523626021001c79ddbbce55e8ea997c
geraldoblc <geraldoblcost@gmail.com>
2024-07-24 13:05:53 -0300

    Update CSS styles, fix image margins, and improve header layout

--- js/language.js
+++ js/language.js
@@ -1,12 +1,40 @@
 const languageTexts = {
     'pt-br': {
         contacts: 'CONTATOS',
-        welcomeTitleTI: 'Bem-vindo(a) à divisão de TI da PSTG!',
-        welcomeTitleEE: 'Bem-vindo(a) à divisão de EE da PSTG!',
         aboutUsTitle: 'QUEM SOMOS',
         doingTitle: 'O QUE FAZEMOS',
         benefitsTitle: 'PRINCIPAIS BENEFÍCIOS PARA NOSSOS CLIENTES',
         projectsTitle: 'PROJETOS',
+        partnersTitle: 'PARCEIROS',
+        contactUsTitle: 'Contate-nos',
+
+        welcomeTitleTI: 'Bem-vindo(a) à divisão de TI da PSTG!',
+        aboutUsParagraphTI: 'A PSTG orgulhosamente apresenta sua Divisão de TI (TI-PSTG), especializada no desenvolvimento de software para diversas áreas da engenharia. Com uma equipe de desenvolvedores altamente capacitados e engenheiros multidisciplinares, incluindo mestres e doutores com mais de 20 anos de experiência, mais de 200 projetos concluídos, nossa missão é liderar a inovação e eficiência em seus projetos de engenharia.',
+
+        doingParagraphTI: 'Nosso trabalho é impulsionado por tecnologias avançadas, especialmente no uso de Inteligência Artificial (IA), para desenvolver soluções inovadoras e personalizadas para nossos clientes.',
+        doingTitle1TI: 'Automatização de Processos:',
+        doingParagraph1aTI: 'Desenvolvemos soluções que automatizam processos em estudos elétricos, projetos de linhas de transmissão e subestações.',
+        doingParagraph1bTI: 'Criamos aplicativos customizados para concessionárias de Transmissão, Geração, Distribuição, Armazenamento de Energia e Hidrogênio Verde.',
+        doingTitle2TI: 'Desenvolvimento de Software com IA:',
+        doingParagraph2aTI: 'Utilizamos ferramentas de IA para otimização de software, análise preditiva e processamento de grandes volumes de dados.',
+        doingParagraph2bTI: 'Aplicamos algoritmos de Machine Learning, Processamento de Linguagem Natural (NLP) e Redes Neurais Artificiais para soluções avançadas.',
+        doingTitle3TI: 'Metodologia Ágil e Inovação:',
+        doingParagraph3aTI: 'Adotamos metodologias ágeis para garantir flexibilidade e rápida adaptação às necessidades do cliente.',
+        doingParagraph3bTI: 'Nosso processo inclui levantamento de requisitos, design e arquitetura de software, desenvolvimento e implementação, testes e validação, e implantação com suporte contínuo.',
+
+        benefitsTitle1TI: 'Eficiência e Confiabilidade:',
+        benefitsParagraph1aTI: 'Nossas soluções automatizadas melhoram significativamente a eficiência operacional, reduzindo o tempo e os custos associados a processos manuais.',
+        benefitsParagraph1bTI: 'A utilização de IA garante previsões precisas e detecção de anomalias, aumentando a confiabilidade dos sistemas.',
+        benefitsTitle2TI: 'Inovação Tecnológica:',
+        benefitsParagraph2aTI: 'Com uma abordagem inovadora e o uso de tecnologias de ponta, oferecemos soluções que colocam nossos clientes na vanguarda do setor.',
+        benefitsParagraph2bTI: 'Nossos softwares são projetados para serem escaláveis e adaptáveis, acompanhando o crescimento e as mudanças nas necessidades do cliente.',
+        benefitsTitle3TI: 'Suporte Contínuo e Personalizado:',
+        benefitsParagraph3aTI: 'Oferecemos suporte contínuo após a implantação, garantindo que nossos clientes sempre tenham a assistência necessária para maximizar o uso de nossas soluções.',
+        benefitsParagraph3bTI: 'Nossa equipe trabalha em estreita colaboração com os clientes para entender suas necessidades específicas e oferecer soluções personalizadas que atendam exatamente às suas expectativas.',
+
+        contactParagraphTI: 'Estamos à disposição para discutir como nossos serviços podem atender às suas necessidades específicas e contribuir para o sucesso dos seus projetos de engenharia. Vamos transformar suas ideias em realidade!',
+
+        welcomeTitleEE: 'Bem-vindo(a) à divisão de EE da PSTG!',
         aboutUsParagraphEE: 'A divisão de Estudos Elétricos (EE-PSTG) da PSTG foi criada para atender às demandas do setor elétrico em transmissão, geração, distribuição e armazenamento de energia. Composta por uma equipe que reúne profissionais com mais de 20 anos de experiência e jovens talentos, esta divisão combina tradição e inovação para oferecer serviços que atendem aos mais altos padrões de prazo, qualidade e custo, tornando-se uma empresa de consultoria competitiva tanto no mercado nacional quanto internacional.',
         
         doingParagraphEE: 'Executamos uma ampla gama de estudos elétricos.',
@@ -50,17 +78,45 @@ const languageTexts = {
 
         projectsInProgressEE: 'EM ANDAMENTO',
         detailsEE: 'Detalhes...',
-        contactUsTitleEE: 'Contate-nos'
+        contactParagraphEE: 'Estamos à disposição para discutir como nossos serviços podem atender às suas necessidades específicas e contribuir para o sucesso dos seus projetos de engenharia.'
     },
 
     'en': {
         contacts: 'CONTACTS',
-        welcomeTitleTI: 'Welcome to the PSTG TI division!',
-        welcomeTitleEE: 'Welcome to the PSTG EE division!',
         aboutUsTitle: 'ABOUT US',
         doingTitle: 'WHAT WE DO',
         benefitsTitle: 'KEY BENEFITS FOR OUR CLIENTS',
         projectsTitle: 'PROJECTS',
+        partnersTitle: 'PARTNERS',
+        contactUsTitle: 'Contact Us',
+
+        welcomeTitleTI: 'Welcome to the PSTG TI division!',
+        aboutUsParagraphTI: 'PSTG proudly presents its IT Studies division (TI-PSTG), specialized in software development for various engineering areas. With a team of highly skilled developers and multidisciplinary engineers, including masters and doctors with over 20 years of experience, more than 200 completed projects, our mission is to lead innovation and efficiency in your engineering projects.',
+
+        doingParagraphTI: 'Our work is driven by advanced technologies, especially in the use of Artificial Intelligence (AI), to develop innovative and customized solutions for our clients.',
+        doingTitle1TI: 'Process Automation:',
+        doingParagraph1aTI: 'We develop solutions that automate processes in electrical studies, transmission line projects, and substations.',
+        doingParagraph1bTI: 'We create custom applications for Transmission, Generation, Distribution, Energy Storage, and Green Hydrogen utilities.',
+        doingTitle2TI: 'Software Development with AI:',
+        doingParagraph2aTI: 'We use AI tools for software optimization, predictive analysis, and processing large volumes of data.',
+        doingParagraph2bTI: 'We apply Machine Learning algorithms, Natural Language Processing (NLP), and Artificial Neural Networks for advanced solutions.',
+        doingTitle3TI: 'Agile Methodology and Innovation:',
+        doingParagraph3aTI: 'We adopt agile methodologies to ensure flexibility and quick adaptation to customer needs.',
+        doingParagraph3bTI: 'Our process includes requirements gathering, software design and architecture, development and implementation, testing and validation, and deployment with continuous support.',
+
+        benefitsTitle1TI: 'Efficiency and Reliability:',
+        benefitsParagraph1aTI: 'Our automated solutions significantly improve operational efficiency, reducing time and costs associated with manual processes.',
+        benefitsParagraph1bTI: 'The use of AI guarantees accurate forecasts and anomaly detection, increasing the reliability of systems.',
+        benefitsTitle2TI: 'Technological Innovation:',
+        benefitsParagraph2aTI: 'With an innovative approach and the use of cutting-edge technologies, we offer solutions that put our clients at the forefront of the industry.',
+        benefitsParagraph2bTI: 'Our software is designed to be scalable and adaptable, keeping pace with growth and changing customer needs.',
+        benefitsTitle3TI: 'Continuous and Customized Support:',
+        benefitsParagraph3aTI: 'We offer continuous support after deployment, ensuring that our clients always have the assistance needed to maximize the use of our solutions.',
+        benefitsParagraph3bTI: 'Our team works closely with clients to understand their specific needs and offer customized solutions that meet their expectations.',
+
+        contactParagraphTI: 'We are available to discuss how our services can meet your specific needs and contribute to the success of your engineering projects. Let"s turn your ideas into reality!',
+
+        welcomeTitleEE: 'Welcome to the PSTG EE division!',
         aboutUsParagraphEE: 'The PSTG Electrical Studies division (EE-PSTG) was created to meet the demands of the electricity sector in transmission, generation, distribution and energy storage. Made up of a team that brings together professionals with more than 20 years experience and young talents, this division combines tradition and innovation to offer services that meet the highest standards in terms of time, quality and cost, making it a competitive consulting firm in both the national and international markets.',
         
         doingParagraphEE: 'We carry out a wide range of electrical studies.',
@@ -104,17 +160,45 @@ const languageTexts = {
 
         projectsInProgressEE: 'IN PROGRESS',
         detailsEE: 'Details...',
-        contactUsTitleEE: 'Contact Us'
+        contactParagraphEE: 'We are available to discuss how our services can meet your specific needs and contribute to the success of your engineering projects.'
     },
 
     'es': {
         contacts: 'CONTACTOS',
-        welcomeTitleTI: '¡Bienvenido a la división de TI de PSTG!',
-        welcomeTitleEE: '¡Bienvenido a la división de EE de PSTG!',
         aboutUsTitle: 'QUIÉNES SOMOS',
         doingTitle: 'LO QUE HACEMOS',
         benefitsTitle: 'PRINCIPALES BENEFICIOS PARA NUESTROS CLIENTES',
         projectsTitle: 'PROYECTOS',
+        partnersTitle: 'ASOCIADOS',
+        contactUsTitle: 'Contáctenos',
+
+        welcomeTitleTI: '¡Bienvenido a la división de TI de PSTG!',
+        aboutUsParagraphTI: 'PSTG presenta con orgullo su División de TI (TI-PSTG), especializada en desarrollo de software para diversas áreas de la ingeniería. Con un equipo de desarrolladores altamente calificados e ingenieros multidisciplinarios, entre maestros y doctores con más de 20 años de experiencia, más de 200 proyectos completados, nuestra misión es liderar la innovación y la eficiencia en sus proyectos de ingeniería.',
+
+        doingParagraphTI: 'Nuestro trabajo es impulsado por tecnologías avanzadas, especialmente en el uso de Inteligencia Artificial (IA), para desarrollar soluciones innovadoras y personalizadas para nuestros clientes.',
+        doingTitle1TI: 'Automatización de Procesos:',
+        doingParagraph1aTI: 'Desarrollamos soluciones que automatizan procesos en estudios eléctricos, proyectos de líneas de transmisión y subestaciones.',
+        doingParagraph1bTI: 'Creamos aplicaciones personalizadas para servicios de transmisión, generación, distribución, almacenamiento de energía e hidrógeno verde.',
+        doingTitle2TI: 'Desarrollo de Software con IA:',
+        doingParagraph2aTI: 'Utilizamos herramientas de IA para la optimización de software, análisis predictivo y procesamiento de grandes volúmenes de datos.',
+        doingParagraph2bTI: 'Aplicamos algoritmos de Machine Learning, Procesamiento de Lenguaje Natural (NLP) y Redes Neuronales Artificiales para soluciones avanzadas.',
+        doingTitle3TI: 'Metodología Ágil e Innovación:',
+        doingParagraph3aTI: 'Adoptamos metodologías ágiles para garantizar flexibilidad y rápida adaptación a las necesidades del cliente.',
+        doingParagraph3bTI: 'Nuestro proceso incluye la recopilación de requisitos, diseño y arquitectura de software, desarrollo e implementación, pruebas y validación, y despliegue con soporte continuo.',
+
+        benefitsTitle1TI: 'Eficiencia y Confiabilidad:',
+        benefitsParagraph1aTI: 'Nuestras soluciones automatizadas mejoran significativamente la eficiencia operativa, reduciendo el tiempo y los costos asociados a procesos manuales.',
+        benefitsParagraph1bTI: 'El uso de IA garantiza pronósticos precisos y detección de anomalías, aumentando la confiabilidad de los sistemas.',
+        benefitsTitle2TI: 'Innovación Tecnológica:',
+        benefitsParagraph2aTI: 'Con un enfoque innovador y el uso de tecnologías de vanguardia, ofrecemos soluciones que ponen a nuestros clientes a la vanguardia de la industria.',
+        benefitsParagraph2bTI: 'Nuestros softwares están diseñados para ser escalables y adaptables, acompañando el crecimiento y los cambios en las necesidades del cliente.',
+        benefitsTitle3TI: 'Soporte Continuo y Personalizado:',
+        benefitsParagraph3aTI: 'Ofrecemos soporte continuo después de la implementación, garantizando que nuestros clientes siempre tengan la asistencia necesaria para maximizar el uso de nuestras soluciones.',
+        benefitsParagraph3bTI: 'Nuestro equipo trabaja en estrecha colaboración con los clientes para entender sus necesidades específicas y ofrecer soluciones personalizadas que cumplan exactamente con sus expectativas.',
+
+        contactParagraphTI: 'Estamos disponibles para discutir cómo nuestros servicios pueden satisfacer sus necesidades específicas y contribuir al éxito de sus proyectos de ingeniería. ¡Vamos a convertir sus ideas en realidad!',
+
+        welcomeTitleEE: '¡Bienvenido a la división de EE de PSTG!',
         aboutUsParagraphEE: 'PSTG presenta con orgullo su División de Estudios Eléctricos (EE-PSTG), especializada en desarrollo de software para diversas áreas de la ingeniería. Con un equipo de desarrolladores altamente calificados e ingenieros multidisciplinarios, entre maestros y doctores con más de 20 años de experiencia, más de 200 proyectos completados, nuestra misión es liderar la innovación y la eficiencia en sus proyectos de ingeniería.',
         
         doingParagraphEE: 'Realizamos una amplia gama de estudios eléctricos.',
@@ -158,13 +242,13 @@ const languageTexts = {
 
         projectsInProgressEE: 'EN PROGRESO',
         detailsEE: 'Detalles...',
-        contactUsTitleEE: 'Contáctenos'
+        contactParagraphEE: 'Estamos disponibles para discutir cómo nuestros servicios pueden satisfacer sus necesidades específicas y contribuir al éxito de sus proyectos de ingeniería.'
     }
 };
 
-function translateline(tag, texto) {
+function translateline(tag, text) {
     if (document.querySelector(tag) !== null) {
-        document.querySelector(tag).textContent = texto;
+        document.querySelector(tag).textContent = text;
     }
 }
 
@@ -173,15 +257,43 @@ function updateLanguage(language) {
     const texts = languageTexts[language];
     translateline('a[href="#contacts"]', texts.contacts);
     translateline('#contacts', texts.contacts);
-    translateline('#welcometi h1', texts.welcomeTitleTI);
-    translateline('#welcomeee h1', texts.welcomeTitleEE);
     translateline('.initial h1', texts.aboutUsTitle);
-    translateline('#aboutusparagraphee', texts.aboutUsParagraphEE);
     translateline('.doing h1', texts.doingTitle);
-    translateline('#benefitsee h1', texts.benefitsTitle);
-    translateline('#projectsee h1', texts.projectsTitle);
+    translateline('.benefits h1', texts.benefitsTitle);
+    translateline('.projects h1', texts.projectsTitle);
+    translateline('.partners h1', texts.partnersTitle);
+    translateline('.final h1', texts.contactUsTitle);
 
-    translateline('.doing p', texts.doingParagraphEE);
+    translateline('#welcometi h1', texts.welcomeTitleTI);
+    translateline('#aboutusparagraphti', texts.aboutUsParagraphTI);
+
+    translateline('#doingparagraph0ti', texts.doingParagraphTI);
+    translateline('#doingtitle1ti', texts.doingTitle1TI);
+    translateline('#doingparagraph1ati', texts.doingParagraph1aTI);
+    translateline('#doingparagraph1bti', texts.doingParagraph1bTI);
+    translateline('#doingtitle2ti', texts.doingTitle2TI);
+    translateline('#doingparagraph2ati', texts.doingParagraph2aTI);
+    translateline('#doingparagraph2bti', texts.doingParagraph2bTI);
+    translateline('#doingtitle3ti', texts.doingTitle3TI);
+    translateline('#doingparagraph3ati', texts.doingParagraph3aTI);
+    translateline('#doingparagraph3bti', texts.doingParagraph3bTI);
+
+    translateline('#benefitstitle1ti', texts.benefitsTitle1TI);
+    translateline('#benefitsparagraph1ati', texts.benefitsParagraph1aTI);
+    translateline('#benefitsparagraph1bti', texts.benefitsParagraph1bTI);
+    translateline('#benefitstitle2ti', texts.benefitsTitle2TI);
+    translateline('#benefitsparagraph2ati', texts.benefitsParagraph2aTI);
+    translateline('#benefitsparagraph2bti', texts.benefitsParagraph2bTI);
+    translateline('#benefitstitle3ti', texts.benefitsTitle3TI);
+    translateline('#benefitsparagraph3ati', texts.benefitsParagraph3aTI);
+    translateline('#benefitsparagraph3bti', texts.benefitsParagraph3bTI);
+
+    translateline('#contactparagraphti', texts.contactParagraphTI);
+
+    translateline('#welcomeee h1', texts.welcomeTitleEE);
+    translateline('#aboutusparagraphee', texts.aboutUsParagraphEE);
+
+    translateline('#doingparagraphee', texts.doingParagraphEE);
     translateline('#doingee li:nth-child(1) li p', texts.doingStudy1EE);
     translateline('#doingee li:nth-child(2) li p', texts.doingStudy2EE);
     translateline('#doingee li:nth-child(3) li p', texts.doingStudy3EE);
@@ -221,8 +333,9 @@ function updateLanguage(language) {
     translateline('#projectskeyword10', texts.projectskeyword10);
 
     translateline('#projectsee .present h2', texts.projectsInProgressEE);
+    
     translateline('#details', texts.detailsEE);
-    translateline('.final h1', texts.contactUsTitleEE);
+    translateline('#contactparagraphee', texts.contactParagraphEE);
 }
 
 // Evento para alterar o idioma ao selecionar uma opção no menu
